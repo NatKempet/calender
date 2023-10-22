3297636cf5707652f71f9b83d6195f5e6cbe1827
Nat <nat_kempet@cmu.ac.th>
2023-10-22 15:16:36 +0700

    added more

--- Calender/src/App.tsx
+++ Calender/src/App.tsx
@@ -2,8 +2,16 @@ import { format } from "date-fns";
 import Calender from "./calender/Calender";
 import { useState, useEffect } from "react";
 import { db } from "./config/Firebase";
-import { getDocs, collection, addDoc,deleteDoc,doc } from "firebase/firestore";
+import {
+  getDocs,
+  collection,
+  addDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 import Modal from "./Modal";
+import downArrow from "./pictures/61932.png";
+import upArrow from "./pictures/56841.png";
 
 const App = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
@@ -14,6 +22,7 @@ const App = () => {
   const [month, setMonth] = useState(0);
   const [year, setYear] = useState(0);
   const [activity, setActivity] = useState("");
+  const [selected, setSelected] = useState(false);
   const Month = [
     "January",
     "February",
@@ -41,7 +50,7 @@ const App = () => {
     }
   };
 
-  const deleteEvents = async (id:any) => {
+  const deleteEvents = async (id: any) => {
     const eventdoc = doc(db, "todo", id);
     await deleteDoc(eventdoc);
   };
@@ -52,15 +61,25 @@ const App = () => {
 
   const handleSetToday = () => setCurrentDate(new Date());
 
-  const getData = async(day:any,month:any,year:any, event:any) =>{
-    await addDoc(eventsCollection, {Day:day, Month:month, Year:year,Activity:event});
+  const getData = async (day: any, month: any, year: any, event: any) => {
+    await addDoc(eventsCollection, {
+      Day: day,
+      Month: month,
+      Year: year,
+      Activity: event,
+    });
 
     getEvents();
-  }
-
+  };
+  const toggle = () => {
+    if (selected == false) {
+      return setSelected(true);
+    }
+    return setSelected(false);
+  };
   return (
     <>
-      {openModal && <Modal closeModal={setOpenModal} getData = {getData}/>}
+      {openModal && <Modal closeModal={setOpenModal} getData={getData} />}
       <div className="mt-32 flex flex-col items-center gap-8">
         <div className="flex flex-col items-center gap-4">
           <p className="text-[19px]">
@@ -73,7 +92,11 @@ const App = () => {
             Today
           </button>
         </div>
-        <Calender value={currentDate} onChange={setCurrentDate} events={events}/>
+        <Calender
+          value={currentDate}
+          onChange={setCurrentDate}
+          events={events}
+        />
         <div className="border w-[300px] flex flex-col divide-y divide-slate-700">
           <div
             className="flex justify-center hover:bg-gray-200 active:bg-gray-400 cursor-pointer"
@@ -83,18 +106,39 @@ const App = () => {
           >
             <b className="text-[30px]">Add Events</b>
           </div>
-          {events.map((index: any) => (
-            <div className="flex flex-col items-center">
-              <br />
-              <h1 className="text-center text-[20px] font-[600]" key = {index.Day}>
-                {index.Day} {Month[index.Month - 1]} {index.Year}
-              </h1>
-              <p className="text-center text-[15px]" key = {index.Activity}>{index.Activity}</p>
-              <br />
-              <button onClick={() => deleteEvents(index.id)} className="bg-rose-700 text-white rounded-lg">Delete Event</button>
-              <br />
-            </div>
-          ))}
+          <div
+            className="grid justify-items-center hover:bg-gray-200 active:bg-gray-400 cursor-pointer"
+            onClick={() => toggle()}
+          >
+            <img
+              src={selected == true ? upArrow : downArrow}
+              className="h-8 w-30"
+            />
+          </div>
+          {events.map((index: any) =>
+            selected == true ? (
+              <div className="flex flex-col items-center">
+                <br />
+                <h1
+                  className="text-center text-[20px] font-[600]"
+                  key={index.Day}
+                >
+                  {index.Day} {Month[index.Month - 1]} {index.Year}
+                </h1>
+                <p className="text-center text-[15px]" key={index.Activity}>
+                  {index.Activity}
+                </p>
+                <br />
+                <button
+                  onClick={() => deleteEvents(index.id)}
+                  className="bg-rose-700 text-white rounded-lg"
+                >
+                  Delete Event
+                </button>
+                <br />
+              </div>
+            ) : null
+          )}
         </div>
       </div>
     </>
